e8fc72afaba5ddf2bd31f68efd09afc1f75ada80
Caio Souza <savio.carvalho@tinnova.com.br>
2019-09-10 16:13:16 -0300

    SpringBoot Parte 2 - Simplificando a paginação e ordenação.

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport //habilita o spring pegar os dados da url para o spring data
 public class ForumApplication {
 
 	public static void main(String[] args) {

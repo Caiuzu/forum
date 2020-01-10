1f1a8be89aab9147d86e692fe7c1698333463c98
Caio Souza <caio.7s@outlook.com>
2020-01-10 13:29:54 -0300

    Gerando token com JWT.

--- src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /*todas configurações de segurança ficam aqui nessa classe*/
@@ -20,21 +23,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private AutenticacaoService AutenticacaoService;
 
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception{
+        return super.authenticationManager();
+    }
+
     @Override /*metodo que configura autenticação*/
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(AutenticacaoService).passwordEncoder(new BCryptPasswordEncoder()); // encryt senha
     }
 
-    @Override /*configuraçeõs de autorização (quyem pode acessar url, perfil de acesso)*/
+    @Override /*configuraçeõs de autorização (quyem pode acessar url, perfil de acesso), todas novas url devem ser adicionadas aqui*/
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
         .antMatchers(HttpMethod.GET,"/topicos").permitAll()
         .antMatchers(HttpMethod.GET,"/topicos/*").permitAll()
+        .antMatchers(HttpMethod.POST,"/auth").permitAll()
         .anyRequest().authenticated()
-        .and().formLogin();//formulario padrao do spring
+        .and().csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //.and().formLogin();//formulario padrao do spring
     }
 
-    @Override /*configuraçãod e recursos estáticos (css, javascript, img, etc)*/
+    @Override /*configuração de recursos estáticos (css, javascript, img, etc)*/
     public void configure(WebSecurity web) throws Exception {
     // Se o front fosse integrado, seria configurado para nao ser bloqueado o front aqui, não ser interceptado na area de segurança
     }

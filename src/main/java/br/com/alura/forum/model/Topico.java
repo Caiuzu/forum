f0eb3b4dbb8e0e8a7f8decc6c5dbbd29bf67fbe7
Caio Souza <caio.7s@outlook.com>
2021-02-15 18:42:55 -0300

    Sonar - Configurando sonarcloud em repositório git e gerando tutorial

--- src/main/java/br/com/alura/forum/model/Topico.java
+++ src/main/java/br/com/alura/forum/model/Topico.java
@@ -8,125 +8,123 @@ import java.util.List;
 @Entity
 public class Topico {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String titulo;
-	private String mensagem;
-	private LocalDateTime dataCriacao = LocalDateTime.now();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Enumerated(EnumType.STRING) // status e enum, deve ser gravado no banco como string
-	private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
-	@ManyToOne
-	private Usuario autor;
-	@ManyToOne
-	private Curso curso;
-	@OneToMany(mappedBy = "topico")
-	private List<Resposta> respostas = new ArrayList<>();
+    private LocalDateTime dataCriacao = LocalDateTime.now();
 
-	/*public Topico(String titulo, String mensagem, Curso curso) {
-		this.titulo = titulo;
-		this.mensagem = mensagem;
-		this.curso = curso;
-	}*/
-	/*tinha criado o construtor só para facilitar na hora de dar new e já passar as informações. Não preciso mais,
-	já que estou carregando do banco. Se eu apagar, o Java vai gerar o construtor default sem nenhum parâmetro.*/
+    @Enumerated(EnumType.STRING) // status e enum, deve ser gravado no banco como string
+    private StatusTopico status = StatusTopico.NAO_RESPONDIDO;
 
-	public Topico(){} //contrutor default
+    @ManyToOne
+    private Usuario autor;
 
-	public Topico(String titulo, String mensagem, Curso curso) { // contrutor para converter o form
-		this.titulo = titulo;
-		this.mensagem = mensagem;
-		this.curso = curso;
-	}
+    @ManyToOne
+    private Curso curso;
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    @OneToMany(mappedBy = "topico")
+    private List<Resposta> respostas = new ArrayList<>();
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Topico other = (Topico) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    private String titulo;
 
-	public Long getId() {
-		return id;
-	}
+    private String mensagem;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Topico() {
+    }
 
-	public String getTitulo() {
-		return titulo;
-	}
+    public Topico(String titulo, String mensagem, Curso curso) { // contrutor para converter o form
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+        this.curso = curso;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	public String getMensagem() {
-		return mensagem;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Topico other = (Topico) obj;
+        if (id == null) {
+            return other.id == null;
+        } else {
+            return id.equals(other.id);
+        }
+    }
 
-	public void setMensagem(String mensagem) {
-		this.mensagem = mensagem;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public LocalDateTime getDataCriacao() {
-		return dataCriacao;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setDataCriacao(LocalDateTime dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
+    public String getTitulo() {
+        return titulo;
+    }
 
-	public StatusTopico getStatus() {
-		return status;
-	}
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 
-	public void setStatus(StatusTopico status) {
-		this.status = status;
-	}
+    public String getMensagem() {
+        return mensagem;
+    }
 
-	public Usuario getAutor() {
-		return autor;
-	}
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
 
-	public void setAutor(Usuario autor) {
-		this.autor = autor;
-	}
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
 
-	public Curso getCurso() {
-		return curso;
-	}
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 
-	public void setCurso(Curso curso) {
-		this.curso = curso;
-	}
+    public StatusTopico getStatus() {
+        return status;
+    }
 
-	public List<Resposta> getRespostas() {
-		return respostas;
-	}
+    public void setStatus(StatusTopico status) {
+        this.status = status;
+    }
 
-	public void setRespostas(List<Resposta> respostas) {
-		this.respostas = respostas;
-	}
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
+    public List<Resposta> getRespostas() {
+        return respostas;
+    }
+
+    public void setRespostas(List<Resposta> respostas) {
+        this.respostas = respostas;
+    }
 
 }
